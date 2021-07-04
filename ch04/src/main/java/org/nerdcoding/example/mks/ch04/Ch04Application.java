2a702bba57faf54e74711af0c686305cdcb3b66f
Tobias Koltsch <tobias@koltsch.de>
2021-07-04 10:54:12 +0200

    Rename to ch04 application

--- ch04/src/main/java/org/nerdcoding/example/mks/ch04/Ch04Application.java
+++ ch04/src/main/java/org/nerdcoding/example/mks/ch04/Ch04Application.java
@@ -2,9 +2,9 @@ package org.nerdcoding.example.mks.ch04;
 
 import io.micronaut.runtime.Micronaut;
 
-public class Application {
+public class Ch04Application {
 
     public static void main(String[] args) {
-        Micronaut.run(Application.class, args);
+        Micronaut.run(Ch04Application.class, args);
     }
 }
